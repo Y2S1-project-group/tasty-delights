51209da32d15f6e1da507e434392dd481946ec03
GIHAA <gihansudeeparandimal@gmail.com>
2022-11-07 14:11:15 +0530

    Add order WIP

--- src/main/java/com/util/CartDatabaseUtil.java
+++ src/main/java/com/util/CartDatabaseUtil.java
@@ -14,7 +14,7 @@ public class CartDatabaseUtil implements Cart {
     }
 
     public int getCartId(int cusid) throws Exception {
-        //sessions
+
         String getCartId = "SELECT * FROM cart where cusid = ?";
         int result;
         DatabaseConnection object = DatabaseConnection.getInstance();
@@ -121,6 +121,27 @@ public class CartDatabaseUtil implements Cart {
         return res;
     }
 
+    public int addNewCart(String fname , String lname , String email ) throws Exception {
+        String query = "SELECT id from customer where fname = ? AND lname = ? AND email = ?";
+        int result;
+        DatabaseConnection object = DatabaseConnection.getInstance();
+        Connection conn = object.getConnection();
+        Statement st = conn.createStatement();
+
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, fname);
+        preparedStatement.setString(2, lname);
+        preparedStatement.setString(3, email);
+
+        ResultSet rs = preparedStatement.executeQuery();
+        rs.next();
+        int cusid =  rs.getInt("id");
+        query = String.format("insert into cart (cusid) values(%d)", cusid);
+
+
+        int res = st.executeUpdate(query);
+        return res;
+    }
     public void addOrder(){
         String query = String.format("");
     }
