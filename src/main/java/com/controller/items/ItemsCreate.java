ef07a3025d5c7f80af7a37ef533f86c906a3830e
Pasindu Dinal Sumanasekara <51123579+mrmihi@users.noreply.github.com>
2022-11-09 08:16:31 +0530

    Loggers added to item-create

--- src/main/java/com/controller/items/ItemsCreate.java
+++ src/main/java/com/controller/items/ItemsCreate.java
@@ -12,23 +12,34 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @WebServlet(name = "ItemsCreate", value = "/ItemsCreate")
 public class ItemsCreate extends HttpServlet {
+
+    private static final Logger LOGGER = Logger.getLogger(ItemsCreate.class.getName());
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try{
             ItemDatabaseUtil itemCreate = new ItemDatabaseUtil();
 
             String name = request.getParameter("item-name");
-            double price = Integer.parseInt(request.getParameter("price"));
-            String image = request.getParameter("image");
-            String description = request.getParameter("description");
-            String category = request.getParameter("category");
+            double price = Double.parseDouble(request.getParameter("item-price"));
+            String image = request.getParameter("item-image");
+            String description = request.getParameter("item-description");
+            String category = request.getParameter("item-category");
 
             if (itemCreate.createItems(name, description, category, price,image)){
                 request.setAttribute("createStatus", "success");
+
+                LOGGER.warning("Logger Name: "+LOGGER.getName());
+                LOGGER.warning("Succesfully created the record: " + "[" + name + "," + price + "," + image + "]");
             }else{
                 request.setAttribute("createStatus", "fail");
+
+                LOGGER.warning("Logger Name: "+LOGGER.getName());
+                LOGGER.warning("Failed to create the record!");
             }
 
             RequestDispatcher dis = request.getRequestDispatcher("ItemsGet");
